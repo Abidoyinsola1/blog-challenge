a410c2d907534cfcbe93eaa89f01a6812e9491b7
Doyinsola Adeyemi <ifeobayemi@gmail.com>
2021-09-14 15:38:10 +0100

    Change listening port

--- app.js
+++ app.js
@@ -98,7 +98,6 @@ app.get('/post/:title', (req, res) => {
 
 })
 
-
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
